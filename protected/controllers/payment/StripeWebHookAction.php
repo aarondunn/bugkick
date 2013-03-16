6b608ff764f3a83b9f729e1a8eb85db7c682e2c8
Alex K <kavshirko@gmail.com>
2013-03-16 14:08:24 +0300

    Stripe webhook updated

--- protected/controllers/payment/StripeWebHookAction.php
+++ protected/controllers/payment/StripeWebHookAction.php
@@ -2,14 +2,15 @@
 class StripeWebHookException extends CException {
 }
 /**
- * StripeWebhookAction
+ * StripeWebHookAction
  * 
  *	Next handlers on next events are implemented for Stripe calls at the moment:
  * 
- *	recurring_payment_succeeded
- *	recurring_payment_failed
+ *	invoice.payment_succeeded
+ *	invoice.payment_failed
  *
  * @author f0t0n
+ * @author Alexey Kavshirko
  */
 class StripeWebHookAction extends Action {
 
@@ -20,19 +21,19 @@ class StripeWebHookAction extends Action {
 		parent::__construct($controller, $id);
 		$this->json=null;
 		$this->eventDictionary=array(
-			'recurring_payment_succeeded'=>'recurringPaymentSucceeded',
-			'recurring_payment_failed'=>'recurringPaymentFailed',
-			'subscription_final_payment_attempt_failed'=>'finalAttepmtFailed',
+			'invoice.payment_succeeded'=>'recurringPaymentSucceeded',
+			'invoice.payment_failed'=>'recurringPaymentFailed',
+			//'subscription_final_payment_attempt_failed'=>'finalAttemptFailed',
 		);
 	}
 
 	public function run() {
 		header('Content-Type: text/plain');
-		$this->json=CJSON::decode($this->request->getPost('json'));
-		if(is_array($this->json) && !empty($this->json['event'])
-			&& isset($this->eventDictionary[$this->json['event']])) {
-			$hookMethod=$this->eventDictionary[$this->json['event']];
-			echo 'Stripe raised an event: "', $this->json['event'] , '";';
+		$this->json=CJSON::decode(stripslashes(@file_get_contents('php://input')));
+		if(is_array($this->json) && !empty($this->json['type'])
+			&& isset($this->eventDictionary[$this->json['type']])) {
+			$hookMethod=$this->eventDictionary[$this->json['type']];
+			echo 'Stripe raised an event: "', $this->json['type'] , '";';
 			try {
 				$this->$hookMethod();
 			} catch(StripeWebHookException $ex) {
@@ -44,22 +45,36 @@ class StripeWebHookAction extends Action {
 	}
 	
 	/**
-	 * Handles Stripe's notification about successfull subscription payment.
+	 * Handles Stripe's notification about successful subscription payment.
 	 * 
 	 */
 	protected function recurringPaymentSucceeded() {
-		if(!$this->json['payment']['success']
-			|| !$this->json['invoice']['paid']) {
+		if(!$this->json['data']['object']['paid']) {
 			return;
 		}
 		$stripeCustomer=$this->getStripeCustomer();
-		$stripeCustomer->last_payment_time=$this->json['invoice']['date'];
+		$stripeCustomer->last_payment_time=$this->json['data']['object']['date'];
 		$stripeCustomer->is_canceled=0;
 		$stripeCustomer->notified_at=null;
 		$stripeCustomer->save();
 	}
-	
-	protected function recurringPaymentFailed() {
+
+    /**
+     * Checks if Stripe's notification when attempt to charge was failed <br />
+     * and if it was final, we need to cancel user's subscription
+     *
+     */
+    protected function recurringPaymentFailed() {
+        //check if it's the last attempt to charge(in that case 'next_payment_attempt' is null)
+        if(!empty($this->json['data']['object']['next_payment_attempt'])) {
+            return;
+        }
+        $stripeCustomer=$this->getStripeCustomer();
+        $stripeCustomer->is_canceled=1;
+        if($this->notifySubscriptionCanceled($stripeCustomer)) {
+            $stripeCustomer->notified_at=time();
+        }
+        $stripeCustomer->save();
 	}
 	
 	/**
@@ -67,7 +82,7 @@ class StripeWebHookAction extends Action {
 	 * on his subscription is failed.
 	 * 
 	 */
-	protected function finalAttepmtFailed() {
+	protected function finalAttemptFailed() {
 		$stripeCustomer=$this->getStripeCustomer();
 		$stripeCustomer->is_canceled=1;
 		if($this->notifySubscriptionCanceled($stripeCustomer)) {
@@ -77,7 +92,7 @@ class StripeWebHookAction extends Action {
 	}
 	
 	protected function getStripeCustomer() {
-		$stripeCustomer=StripeCustomer::model()->findByPk($this->json['customer']);
+		$stripeCustomer=StripeCustomer::model()->findByPk($this->json['data']['object']['customer']);
 		if(empty($stripeCustomer)) {
 			throw new StripeWebHookException('Wrong request');
 		}
@@ -87,137 +102,117 @@ class StripeWebHookAction extends Action {
 	/**
 	 * @todo Send an e-mail to user and return true on success, false otherwise.
 	 * 
-	 * @return boolean is user notified about his subscription cancelation.
+	 * @return boolean is user notified about his subscription cancellation.
 	 */
 	protected function notifySubscriptionCanceled() {
 		return false;
 	}
 }
 
-//	The example of request from Stripe for recurring_payment_succeeded:
-//		{
-//		  "payment": {
-//			"card": {
-//			  "type": "Visa",
-//			  "last4": "2424",
-//			  "id": "Ncqojy1Sgdw"
-//			},
-//			"time": 1324950721,
-//			"success": true
-//		  },
-//		  "customer": "cus_fTcnTQVUUzpKEQ",
-//		  "event": "recurring_payment_succeeded",
-//		  "invoice": {
-//			"paid": true,
-//			"period_start": 1325096899,
-//			"attempted": true,
-//			"closed": true,
-//			"customer": "cus_fTcnTQVUUzpKEQ",
-//			"lines": {
-//			  "subscriptions": [
-//				{
-//				  "period": {
-//					"end": 1327775299,
-//					"start": 1325096899
-//				  },
-//				  "plan": {
-//					"object": "plan",
-//					"amount": 500,
-//					"name": "BugKick Pro Plan",
-//					"id": "bugkick_plan_pro",
-//					"livemode": false,
-//					"currency": "usd",
-//					"interval": "month"
-//				  },
-//				  "amount": 500
-//				}
-//			  ]
-//			},
-//			"period_end": 1325096899,
-//			"charge": "ch_wLlQxPMGuIbYKl",
-//			"object": "invoice",
-//			"date": 1325096899,
-//			"total": 500,
-//			"subtotal": 500,
-//			"id": "in_9a8zlFvZyuV6vc",
-//			"livemode": false
-//		  },
-//		  "livemode": false
-//		}
+//	The example of request from Stripe for invoice.payment_succeeded:
+//{
+//  "object": "event",
+//  "type": "invoice.payment_succeeded",
+//  "created": 1326853478,
+//  "livemode": false,
+//  "data": {
+//    "object": {
+//      "object": "invoice",
+//      "next_payment_attempt": null,
+//      "livemode": false,
+//      "attempted": true,
+//      "lines": {
+//        "subscriptions": [
+//          {
+//            "amount": 5995,
+//            "period": {
+//              "end": 1350900376,
+//              "start": 1348308376
+//            },
+//            "plan": {
+//              "amount": 2995,
+//              "object": "plan",
+//              "livemode": false,
+//              "trial_period_days": null,
+//              "name": "Support Services",
+//              "interval": "month",
+//              "id": "2",
+//              "currency": "usd"
+//            }
+//          }
+//        ]
+//      },
+//      "subtotal": 5995,
+//      "amount_due": 5995,
+//      "period_start": 1345629916,
+//      "starting_balance": 0,
+//      "paid": true,
+//      "attempt_count": 1,
+//      "ending_balance": 0,
+//      "discount": null,
+//      "closed": true,
+//      "date": 1345629991,
+//      "id": "in_00000000000000",
+//      "period_end": 1345629976,
+//      "total": 5995,
+//      "customer": "cus_00000000000000",
+//      "currency": "usd",
+//      "charge": "_00000000000000"
+//    }
+//  },
+//  "id": "evt_00000000000000"
+//}
 
-//	The example of request from Stripe for recurring_payment_failed:
-//		{
-//		  "attempt": 1,
-//		  "payment": {
-//			"card": {
-//			  "type": "Visa",
-//			  "last4": "2424",
-//			  "id": "Ncqojy1Sgdw"
-//			},
-//			"time": 1324950721,
-//			"success": false
-//		  },
-//		  "customer": "cus_fTcnTQVUUzpKEQ",
-//		  "event": "recurring_payment_failed",
-//		  "invoice": {
-//			"paid": false,
-//			"period_start": 1325096899,
-//			"attempted": true,
-//			"closed": true,
-//			"customer": "cus_fTcnTQVUUzpKEQ",
-//			"lines": {
-//			  "subscriptions": [
-//				{
-//				  "period": {
-//					"end": 1327775299,
-//					"start": 1325096899
-//				  },
-//				  "plan": {
-//					"object": "plan",
-//					"amount": 500,
-//					"name": "BugKick Pro Plan",
-//					"id": "bugkick_plan_pro",
-//					"livemode": false,
-//					"currency": "usd",
-//					"interval": "month"
-//				  },
-//				  "amount": 500
-//				}
-//			  ]
-//			},
-//			"period_end": 1325096899,
-//			"charge": "ch_wLlQxPMGuIbYKl",
-//			"object": "invoice",
-//			"date": 1325096899,
-//			"total": 500,
-//			"subtotal": 500,
-//			"id": "in_9a8zlFvZyuV6vc",
-//			"livemode": false
-//		  },
-//		  "livemode": false
-//		}
-
-//	The example of request from Stripe for subscription_final_payment_attempt_failed:
-//		{
-//		  "customer": "cus_MOohbisUxZs0rU",
-//		  "subscription": {
-//			"ended_at": 1327774553,
-//			"status": "canceled",
-//			"customer": "cus_MOohbisUxZs0rU",
-//			"start": 1325096153,
-//			"object": "subscription",
-//			"plan": {
-//			  "object": "plan",
-//			  "amount": 500,
-//			  "name": "BugKick Pro Plan",
-//			  "id": "bugkick_plan_pro",
-//			  "livemode": false,
-//			  "currency": "usd",
-//			  "interval": "month"
-//			},
-//			"current_period_start": 1325096153,
-//			"current_period_end": 1327774553
-//		  },
-//		  "event": "subscription_final_payment_attempt_failed",
-//		  "livemode": false
-//		}
+//	The example of request from Stripe for invoice.payment_failed:
+//{
+//  "object": "event",
+//  "type": "invoice.payment_failed",
+//  "created": 1326853478,
+//  "livemode": false,
+//  "data": {
+//    "object": {
+//      "object": "invoice",
+//      "next_payment_attempt": null,
+//      "livemode": false,
+//      "attempted": true,
+//      "lines": {
+//        "subscriptions": [
+//          {
+//            "amount": 5995,
+//            "period": {
+//              "end": 1350900376,
+//              "start": 1348308376
+//            },
+//            "plan": {
+//              "amount": 2995,
+//              "object": "plan",
+//              "livemode": false,
+//              "trial_period_days": null,
+//              "name": "Support Services",
+//              "interval": "month",
+//              "id": "2",
+//              "currency": "usd"
+//            }
+//          }
+//        ]
+//      },
+//      "subtotal": 5995,
+//      "amount_due": 5995,
+//      "period_start": 1345629916,
+//      "starting_balance": 0,
+//      "paid": false,
+//      "attempt_count": 1,
+//      "ending_balance": 0,
+//      "discount": null,
+//      "closed": false,
+//      "date": 1345629991,
+//      "id": "in_00000000000000",
+//      "period_end": 1345629976,
+//      "total": 5995,
+//      "customer": "cus_00000000000000",
+//      "currency": "usd",
+//      "charge": "ch_00000000000000"
+//    }
+//  },
+//  "id": "evt_00000000000000"
+//}
