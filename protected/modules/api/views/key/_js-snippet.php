888b425c43ea88de605010d9f6720d0c6a68db38
Alex K <kavshirko@gmail.com>
2013-03-20 01:21:08 +0300

    #883 Can you come up with a couple variations of the feedback… 020

--- protected/modules/api/views/key/_js-snippet.php
+++ protected/modules/api/views/key/_js-snippet.php
@@ -6,7 +6,8 @@
         var s = d.createElement('script');
         s.type = 'text/javascript';
         s.async = true;
-        s.src = '<?php echo Yii::app()->request->getBaseUrl(true); ?>/BugKick.js/bugkick/base.js';
+        s.src = ('https:' == d.location.protocol ? 'https://' : 'http://')
+            + '<?php echo Yii::app()->params['bugkickApiSettings']['scriptURL']; ?>';
         d.getElementsByTagName('head')[0].appendChild(s);
     })(document);
 </script>
